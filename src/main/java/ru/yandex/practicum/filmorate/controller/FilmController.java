3e0ac84a6ae178a9206fd4f5749407be97b039d1
IVAN_MALSHAKOV <vanodude@yandex.ru>
2023-10-25 23:07:03 +0300

    feat: добавить реализацию FilmController и UserController №2

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/films")
 public class FilmController extends BaseController<Film> {
-    private final static LocalDate START_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    private static final LocalDate START_RELEASE_DATE = LocalDate.of(1895, 12, 28);
 
     @Override
     public void validate(Film film) {
